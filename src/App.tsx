13fe03b71d2bceeab974d3b51ff277d107562650
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-05 12:51:04 -0300

    Add form submission handler to prevent default behavior

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import { useState } from "react";
 export default function App() {
   const [name, setName] = useState("");
 
+  function onSubmit(event: React.FormEvent<HTMLElement>) {
+    event.preventDefault();
+  }
+
   return (
     <div>
       <h1>Evento {name}</h1>
 
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="Nome do evento"
