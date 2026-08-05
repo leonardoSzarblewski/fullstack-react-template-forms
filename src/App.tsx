8ed8001655fadb83948ec8a99edd7c77c8d1492d
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-05 12:43:58 -0300

    Update event name display and handle input change in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import "./App.css"
+import "./App.css";
+
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+
   return (
     <div>
-      <h1>Evento</h1>
+      <h1>Evento {name}</h1>
 
       <form>
-        <input type="text" placeholder="Nome do evento" />
+        <input
+          type="text"
+          placeholder="Nome do evento"
+          onChange={(event) => setName(event.target.value)}
+        />
         <span className="error">Nome é obrigatório</span>
 
         <input type="date" placeholder="Nome do evento" lang="pt-BR" />
@@ -27,5 +35,5 @@ export default function App() {
         <button type="submit">Salvar</button>
       </form>
     </div>
-  )
+  );
 }
