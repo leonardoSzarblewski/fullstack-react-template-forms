ccaebaf6f41afdb3750c17a4278ea08ce5c9b714
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-05 14:53:04 -0300

    Add date field to form and integrate with react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Controller, useForm } from "react-hook-form";
 
 type FormData = {
   name: string;
+  date: string;
 };
 
 export default function App() {
   const { control, handleSubmit } = useForm<FormData>({
     defaultValues: {
       name: "",
+      date: "",
     },
   });
 
@@ -32,7 +34,18 @@ export default function App() {
 
         <span className="error">Nome é obrigatório</span>
 
-        <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+        <Controller
+          control={control}
+          name="date"
+          render={({ field }) => (
+            <input
+              type="date"
+              placeholder="Nome do evento"
+              lang="pt-BR"
+              {...field}
+            />
+          )}
+        />
 
         <select defaultValue="">
           <option value="" disabled>
