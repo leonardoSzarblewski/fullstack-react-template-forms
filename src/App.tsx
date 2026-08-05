e8d52751680f40e0ccb2cafb870ae271d3a055a0
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-05 14:48:59 -0300

    Define FormData type for form data structure and update onSubmit function signature

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import "./App.css";
 
 import { Controller, useForm } from "react-hook-form";
 
+type FormData = {
+  name: string;
+};
+
 export default function App() {
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit } = useForm<FormData>({
     defaultValues: {
       name: "",
     },
   });
 
-  function onSubmit(data) {
+  function onSubmit(data: FormData) {
     console.log(data);
   }
 
